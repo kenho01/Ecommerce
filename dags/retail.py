24769aef89f9b3fc5b61217e9dc5f5206b31ed88
kenho01 <kenho9674@gmail.com>
2024-06-03 16:28:08 +0800

    add transformation code

--- dags/retail.py
+++ dags/retail.py
@@ -7,6 +7,10 @@ from astro import sql as aql
 from astro.files import File
 from astro.sql.table import Table, Metadata
 from astro.constants import FileType
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import RenderConfig
 
 @dag(
     start_date = datetime(2023,1,1),
@@ -36,14 +40,32 @@ def retail():
         input_file=File(
             'gs://uk_online_retail/raw/online_retail.csv',
             conn_id='gcp',
-            filetype=FileType.CSV
+            filetype=FileType.CSV,
         ),
         output_table=Table(
             name='raw_invoices',
             conn_id='gcp',
             metadata=Metadata(schema='retail')
         ),
-        use_native_support=False
+        use_native_support=False,
     )
 
+    # Data quality check for raw data
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_load(scan_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
+        return check(scan_name, checks_subpath)
+    
+    check_load()
+
+    transform = DbtTaskGroup(
+        group_id='transform',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/transform']
+        )
+    )
+    
 retail()
---
+++ include/dbt/target/run/retail/models/transform/dim_customer.sql
@@ -0,0 +1,24 @@
+
+  
+    
+
+    create or replace table `ecommerce-airflow`.`retail`.`dim_customer`
+    
+    
+
+    OPTIONS()
+    as (
+      WITH customer_cte AS (
+	SELECT DISTINCT
+	    to_hex(md5(cast(coalesce(cast(CustomerID as STRING), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(Country as STRING), '_dbt_utils_surrogate_key_null_') as STRING))) as customer_id,
+	    Country AS country
+	FROM `ecommerce-airflow`.`retail`.`raw_invoices`
+	WHERE CustomerID IS NOT NULL
+)
+SELECT
+    t.*,
+	cm.iso
+FROM customer_cte t
+LEFT JOIN `ecommerce-airflow`.`retail`.`country` cm ON t.country = cm.nicename
+    );
+  
